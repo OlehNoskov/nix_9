0762b7308c86ce56b7c256edb9d1a4b3aebb4790
OlehNoskov <olega6000@gmail.com>
2021-10-28 20:48:44 +0300

    fix bag second task first level

--- module_1/src/main/java/first_level/chess/ChessGame.java
+++ module_1/src/main/java/first_level/chess/ChessGame.java
@@ -16,24 +16,34 @@ public class ChessGame {
             String initialPositionСolumn, movePositionСolumn;
 
             System.out.println("=== Установка коня ===");
-            System.out.println("Введите столбец для установки коня по горизонтали:");
+            System.out.println("Введите столбец для установки коня по вертикали:");
             initialPositionСolumn = reader.readLine().toUpperCase();
 
-            System.out.println("Введите номер строки для установки коня по вертикали:");
+            System.out.println("Введите номер строки для установки коня по горизонтали:");
             initialPositionLine = Integer.parseInt(reader.readLine());
+            if (initialPositionLine > 8 || initialPositionLine < 0) {
+                System.out.println("Вы вышли за пределы шахматной доски!"+"\n");
+                ShowFirstLevel.showTasksFirstLevel(reader);
+            }
 
             System.out.println("Ваш конь установлен на позиции: " +
                     initialPositionСolumn + initialPositionLine);
 
             System.out.println("=== Ход конём ===");
-            System.out.println("Введите столбец для хода коня по горизонтали:");
+            System.out.println("Введите столбец для хода коня по вертикали:");
             movePositionСolumn = reader.readLine().toUpperCase();
 
-            System.out.println("Введите номер строки для хода коня по вертикали:");
+            System.out.println("Введите номер строки для хода коня по горизонтали:");
             movePositionLine = Integer.parseInt(reader.readLine());
+
+            if (movePositionLine > 8 || movePositionLine < 0) {
+                System.out.println("Вы вышли за пределы шахматной доски!"+"\n");
+                ShowFirstLevel.showTasksFirstLevel(reader);
+            }
 
             KnightsMove.isMovePossible(ChessBoard.getNumberColumnChessBoard(initialPositionСolumn), initialPositionLine,
                     ChessBoard.getNumberColumnChessBoard(movePositionСolumn), movePositionLine);
+
             ShowFirstLevel.showTasksFirstLevel(reader);
         } catch (NumberFormatException e) {
             System.out.println("Введены некорректные данные!");
@@ -45,3 +55,4 @@ public class ChessGame {
         }
     }
 }
+
