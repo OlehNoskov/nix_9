4d5a97fe1291bb680314cb74f67346d530c56d2d
OlehNoskov <olega6000@gmail.com>
2021-11-18 14:10:25 +0200

    create classes 7 homework

---
+++ hw_7_ionio/src/main/java/ua/com/alevel/Main.java
@@ -0,0 +1,12 @@
+package ua.com.alevel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Working!!!");
+        System.out.println("Working!!!");
+        System.out.println("Working!!!");
+        System.out.println("Working!!!");
+        System.out.println("Working!!!");
+        System.out.println("Working!!!");
+    }
+}
