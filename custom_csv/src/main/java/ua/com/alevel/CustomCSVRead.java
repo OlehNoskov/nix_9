51fb3ff2486cfa741c83cf7e10f6661729f329f9
OlehNoskov <olega6000@gmail.com>
2021-12-02 00:21:35 +0200

    create classes 7 homework

--- custom_csv/src/main/java/ua/com/alevel/CustomCSVRead.java
+++ custom_csv/src/main/java/ua/com/alevel/CustomCSVRead.java
@@ -22,17 +22,4 @@ public class CustomCSVRead {
         }
         return listEntity;
     }
-
-//    public static String[] search(List<String[]> listEntity, String ids) {
-//        String[] entity = new String[];
-//        for (int i = 0; i < listEntity.size(); i++) {
-//            for (int b = 0; b < listEntity.get(i).length; b++) {
-//                if (listEntity.get(i)[b].equals(ids)) {
-//                    entity = listEntity.get(i);
-//                    return entity;
-//                }
-//            }
-//        }
-//        return entity;
-//    }
 }
