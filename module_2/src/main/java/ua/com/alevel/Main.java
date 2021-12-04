db206e9a530c5006c728fcc926e6c8934b957f5f
egorfuntusov <funtushan@gmail.com>
2021-12-04 20:29:03 +0200

    add review

--- module_2/src/main/java/ua/com/alevel/Main.java
+++ module_2/src/main/java/ua/com/alevel/Main.java
@@ -5,6 +5,7 @@ import ua.com.alevel.consolemenu.MenuProgram;
 import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         MenuProgram.run();
     }
