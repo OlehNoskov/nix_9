77f4a2b1319c343494a12ffe6ed23ad6fac0a62c
OlehNoskov <olega6000@gmail.com>
2021-12-13 20:22:28 +0200

    homework 9 2 part

---
+++ hw_9_web_jpa/src/main/java/ua/com/alevel/persistence/dao/impl/StudentDaoImpl.java
@@ -0,0 +1,93 @@
+package ua.com.alevel.persistence.dao.impl;
+
+import org.springframework.stereotype.Repository;
+import ua.com.alevel.datatable.DataTableRequest;
+import ua.com.alevel.datatable.DataTableResponse;
+import ua.com.alevel.persistence.dao.StudentDao;
+import ua.com.alevel.persistence.entity.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.OptimisticLockException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public class StudentDaoImpl implements StudentDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void create(Student entity) {
+        entityManager.persist(entity);
+    }
+
+    @Override
+    public void update(Student entity) {
+        entityManager.merge(entity);
+        entityManager.flush();
+    }
+
+    @Override
+    public void delete(Long id) {
+        int isSuccessful = entityManager.createQuery("delete from Student s where s.id = :id")
+                .setParameter("id", id)
+                .executeUpdate();
+        if (isSuccessful == 0) {
+            throw new OptimisticLockException("student modified concurrently");
+        }
+    }
+
+    @Override
+    public boolean existById(Long id) {
+        Query query = entityManager.createQuery("select count(s.id) from Student s where s.id = :id")
+                .setParameter("id", id);
+        return (Long) query.getSingleResult() == 1;
+    }
+
+    @Override
+    public Student findById(Long id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public DataTableResponse<Student> findAll(DataTableRequest request) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Student> criteriaQuery = criteriaBuilder.createQuery(Student.class);
+        Root<Student> from = criteriaQuery.from(Student.class);
+        if (request.getOrder().equals("desc")) {
+            criteriaQuery.orderBy(criteriaBuilder.desc(from.get(request.getSort())));
+        } else {
+            criteriaQuery.orderBy(criteriaBuilder.asc(from.get(request.getSort())));
+        }
+
+        int page = (request.getPage() - 1) * request.getSize();
+        int size = page + request.getSize();
+
+        List<Student> items = entityManager.createQuery(criteriaQuery)
+                .setFirstResult(page)
+                .setMaxResults(size)
+                .getResultList();
+
+        DataTableResponse<Student> response = new DataTableResponse<>();
+        response.setSort(request.getSort());
+        response.setOrder(request.getOrder());
+        response.setCurrentPage(request.getPage());
+        response.setPageSize(request.getSize());
+        response.setItems(items);
+
+        return response;
+    }
+
+    @Override
+    public long count() {
+        Query query = entityManager.createQuery("select count(s.id) from Student s");
+        return (Long) query.getSingleResult();
+    }
+}
