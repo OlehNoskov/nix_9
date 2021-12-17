8e73f985804b3b47d9f32c3a5aaabc9338981440
OlehNoskov <olega6000@gmail.com>
2021-12-17 17:37:23 +0200

    finish 8 and 9 homework

---
+++ hw_10_repository/src/test/java/ua/Hw10RepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw10RepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
