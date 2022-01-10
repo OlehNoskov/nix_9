df86e9c3036eba151a42d8eeda70bd8e47229d3d
OlehNoskov <olega6000@gmail.com>
2022-01-10 18:35:40 +0200

    working with final project

--- medical_analize/src/main/java/com/project/medicalanalize/web/controller/admin/AdminController.java
+++ medical_analize/src/main/java/com/project/medicalanalize/web/controller/admin/AdminController.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin/dashboard")
+@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping
-    public String dashboard() {
-        return "pages/admin/dashboard";
+    @GetMapping("/doctors")
+    public String allDoctors() {
+        return "pages/admin/admin_doctors_all";
+    }
+
+    @GetMapping("/patients")
+    public String allPatients() {
+        return "pages/admin/admin_patients_all";
+    }
+
+    @GetMapping("/orders")
+    public String allOrders() {
+        return "pages/admin/admin_orders_all";
     }
 }
