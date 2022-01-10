895ec13ba29d32bf0e00e1dbaf08cbc9bab1ec08
OlehNoskov <olega6000@gmail.com>
2022-01-10 17:58:28 +0200

    working with final project

--- medical_analize/src/main/java/com/project/medicalanalize/web/controller/patient/PatientAnalyzeController.java
+++ medical_analize/src/main/java/com/project/medicalanalize/web/controller/patient/PatientAnalyzeController.java
@@ -3,6 +3,7 @@ package com.project.medicalanalize.web.controller.patient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PatientAnalyzeController {
 
     @GetMapping("/all/temp")
-    public String temp(Model model) {
+    public String tempAnalyses(Model model) {
+//        model.addAttribute("analyses", new AnalysesRequestDto());
         return "pages/patient/analyzes/loaded_analyses";
+    }
+
+    @PostMapping("/all/temp")
+    public String uploadAnalyses(Model model) {
+        return "redirect:/patients/dashboard";
     }
 
     @GetMapping("/all/examples")
