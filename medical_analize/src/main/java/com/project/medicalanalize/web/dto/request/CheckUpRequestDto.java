e1496cedc89d40d25642bfe2ebaac04891fe0481
OlehNoskov <olega6000@gmail.com>
2022-01-14 11:36:35 +0200

    working with final project

--- medical_analize/src/main/java/com/project/medicalanalize/web/dto/request/CheckUpRequestDto.java
+++ medical_analize/src/main/java/com/project/medicalanalize/web/dto/request/CheckUpRequestDto.java
@@ -1,71 +1,8 @@
 package com.project.medicalanalize.web.dto.request;
 
-public class CheckUpRequestDto extends RequestDto{
+public class CheckUpRequestDto extends OrderRequestDto{
 
-    private Integer price;
-    private String badHabits;
-    private String drugsTaken;
-    private String chronicDiseases;
-    private String burglaryComplaints;
-    private String hereditary_diseases;
-    private String featuresNutrition;
     private String complaints;
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public String getBadHabits() {
-        return badHabits;
-    }
-
-    public void setBadHabits(String badHabits) {
-        this.badHabits = badHabits;
-    }
-
-    public String getDrugsTaken() {
-        return drugsTaken;
-    }
-
-    public void setDrugsTaken(String drugsTaken) {
-        this.drugsTaken = drugsTaken;
-    }
-
-    public String getChronicDiseases() {
-        return chronicDiseases;
-    }
-
-    public void setChronicDiseases(String chronicDiseases) {
-        this.chronicDiseases = chronicDiseases;
-    }
-
-    public String getBurglaryComplaints() {
-        return burglaryComplaints;
-    }
-
-    public void setBurglaryComplaints(String burglaryComplaints) {
-        this.burglaryComplaints = burglaryComplaints;
-    }
-
-    public String getHereditary_diseases() {
-        return hereditary_diseases;
-    }
-
-    public void setHereditary_diseases(String hereditary_diseases) {
-        this.hereditary_diseases = hereditary_diseases;
-    }
-
-    public String getFeaturesNutrition() {
-        return featuresNutrition;
-    }
-
-    public void setFeaturesNutrition(String featuresNutrition) {
-        this.featuresNutrition = featuresNutrition;
-    }
 
     public String getComplaints() {
         return complaints;
--- medical_analize/src/main/java/com/project/medicalanalize/web/dto/request/ConsultationRequestDto.java
+++ medical_analize/src/main/java/com/project/medicalanalize/web/dto/request/ConsultationRequestDto.java
@@ -1,72 +1,9 @@
 package com.project.medicalanalize.web.dto.request;
 
-public class ConsultationRequestDto extends RequestDto {
+public class ConsultationRequestDto extends OrderRequestDto {
 
-    private Integer price;
-    private String badHabits;
-    private String drugsTaken;
-    private String chronicDiseases;
-    private String burglaryComplaints;
-    private String hereditary_diseases;
-    private String featuresNutrition;
     private String complaints;
     private String file;
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public String getBadHabits() {
-        return badHabits;
-    }
-
-    public void setBadHabits(String badHabits) {
-        this.badHabits = badHabits;
-    }
-
-    public String getDrugsTaken() {
-        return drugsTaken;
-    }
-
-    public void setDrugsTaken(String drugsTaken) {
-        this.drugsTaken = drugsTaken;
-    }
-
-    public String getChronicDiseases() {
-        return chronicDiseases;
-    }
-
-    public void setChronicDiseases(String chronicDiseases) {
-        this.chronicDiseases = chronicDiseases;
-    }
-
-    public String getBurglaryComplaints() {
-        return burglaryComplaints;
-    }
-
-    public void setBurglaryComplaints(String burglaryComplaints) {
-        this.burglaryComplaints = burglaryComplaints;
-    }
-
-    public String getHereditary_diseases() {
-        return hereditary_diseases;
-    }
-
-    public void setHereditary_diseases(String hereditary_diseases) {
-        this.hereditary_diseases = hereditary_diseases;
-    }
-
-    public String getFeaturesNutrition() {
-        return featuresNutrition;
-    }
-
-    public void setFeaturesNutrition(String featuresNutrition) {
-        this.featuresNutrition = featuresNutrition;
-    }
 
     public String getComplaints() {
         return complaints;
