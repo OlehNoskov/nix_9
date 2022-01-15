3bd71b13d618de6464c6e15cc7384f951fe1e85a
OlehNoskov <olega6000@gmail.com>
2022-01-15 23:12:26 +0200

    fix bags

--- medical_analize/src/main/java/com/project/medicalanalize/service/typetranscript/GeneralBloodService.java
+++
@@ -1,7 +0,0 @@
-package com.project.medicalanalize.service.typetranscript;
-
-import com.project.medicalanalize.persistence.entity.order.transcript.GeneralBlood;
-import com.project.medicalanalize.service.BaseCrudService;
-
-public interface GeneralBloodService extends BaseCrudService<GeneralBlood> {
-}
