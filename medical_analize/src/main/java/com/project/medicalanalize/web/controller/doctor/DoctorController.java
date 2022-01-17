2ea771163beb51ee63e84d8e7ae09268545071e2
OlehNoskov <olega6000@gmail.com>
2022-01-17 18:48:39 +0200

    fix bags

--- medical_analize/src/main/java/com/project/medicalanalize/web/controller/doctor/DoctorController.java
+++ medical_analize/src/main/java/com/project/medicalanalize/web/controller/doctor/DoctorController.java
@@ -7,12 +7,9 @@ import com.project.medicalanalize.persistence.entity.user.User;
 import com.project.medicalanalize.web.dto.request.DoctorRequestDto;
 import com.project.medicalanalize.web.dto.response.DoctorResponseDto;
 
-import com.project.medicalanalize.web.dto.response.PageData;
-import com.project.medicalanalize.web.dto.response.TranscriptResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 
 import java.text.ParseException;
 
